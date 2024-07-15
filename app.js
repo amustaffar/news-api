99c4accf88464cef34d4adbe4b5c17b0f0fca4e3
Ahmad Mustaffar <ahmadmustaffar@Ahmads-Laptop.mynet>
2024-07-15 14:25:00 +0100

    3-get-api completed

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const controllers = require("./controllers");
 
+// endpoints from endpoints.json
+const endpoints = require("./endpoints.json");
+
+app.get("/api", (req, res, next) => {
+  //console.log(endpoints);
+  res.status(200).send({ endpoints });
+});
+
 app.get("/api/topics", controllers.getTopics);
 
 app.use((err, req, res, next) => {
