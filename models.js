5be6f7766eae207554b144e2412dd4d1e0ad43ab
Ahmad Mustaffar <ahmadmustaffar@Ahmads-Laptop.mynet>
2024-07-17 14:43:15 +0100

    14-host-application through main branch

--- models.js
+++ models.js
@@ -3,7 +3,7 @@ const format = require("pg-format");
 const db = require("./db/connection");
 
 exports.selectTopics = (query) => {
-  return db.query(`SELECT * FROM topics;`).then(({ rows }) => {
+  return db.query(`SELECT slug, description FROM topics;`).then(({ rows }) => {
     return rows;
   });
 };
